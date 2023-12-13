9b5a9f6cbf47fc77e02e1ff00631558bc734988f
marie <ingabiremarieaimee6@gmail.com>
2023-12-13 10:46:43 -0800

    my push

---
+++ math.js
@@ -0,0 +1,52 @@
+//math methods
+// 1 square root
+console.log(Math.sqrt(25));
+
+//2 absolute
+console.log(Math.abs(-144));
+
+//3 power
+console.log(Math.pow(100,2));
+
+//4 round
+console.log(Math.round(4.06));
+console.log(Math.round(4.6));
+console.log(Math.round(4.3));
+
+//5 ceil
+console.log(Math.ceil(14.3));
+console.log(Math.ceil(14.0));
+console.log(Math.ceil(-14.5));
+
+//6 floor 
+console.log(Math.floor(4.9));
+console.log(Math.floor(-4.6));
+
+//7 exponent
+console.log(Math.exp(4));
+
+//8 random
+console.log(Math.random());
+
+//9 minimum 
+console.log(Math.min(14,53,12));
+
+//10 maximum
+console.log(Math.max(14,10,102));
+
+//11 trunc
+console.log(Math.trunc(4.98));
+
+//12 log
+console.log(Math.log(10));
+console.log(Math.log(0));
+
+//13 cosine
+console.log(Math.cos(100));
+
+//14 sine 
+console.log(Math.sin(100));
+console.log(Math.sin(-100));
+
+//addition of different method
+console.log(Math.abs(-1)+ Math.trunc(2.5));
